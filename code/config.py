884e503f4eca63dde025a86b5fa9d3849f703481
rui-yan <yanrui0498@gmail.com>
2024-10-21 21:08:49 -0700

    update

--- code/config.py
+++
@@ -1,6 +0,0 @@
-DL_MODELS = ['TabMap', 'DeepInsight', '1DCNN', 'IGTD', 'TabNet', 'TabTransformer']
-DL_MODELS_IMAGE_BASED = ['TabMap', 'DeepInsight', 'IGTD']
-DL_MODELS_TRANSFORMER_BASED = ['TabNet', 'TabTransformer']
-ML_MODELS = ['LR', 'RF', 'GB', 'XGB']
-
-METRICS = ["BA", "F1", "MCC"]
